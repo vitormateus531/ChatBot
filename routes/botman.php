820c3469ccfba435e4e1b49e6bae46eff38bef67
Vitor Mateus <vitormateus15@hotmail.com>
2020-09-12 12:56:50 -0300

    ajustes_de_mensagem

--- routes/botman.php
+++ routes/botman.php
@@ -9,74 +9,86 @@ $botman = resolve('botman');
 
 $botman->hears('Oi', function ($bot) {
     $bot->reply('Olá, me chamo Rob, e lhe darei informações sobre os estados do Brasil que voce pesquisar');
-    $bot->reply('Ah! e uma dica, voce pode digitar começando com "estado de/do/da" antes do nome do estado ou com o nome do estado e no final espaço e um ponto');
+    $bot->reply('Ah! e duas dicas, voce pode digitar começando com "estado de/do/da" antes do nome do estado ou somente o nome do estado e no final espaço e um ponto');
+    $bot->reply('e para uma melhor pesquisa digite os estados com acentuação correta'); 
     $bot->reply('qual estado é de seu interesse?');
 });
 
 $botman->hears('Oii', function ($bot) {
     $bot->reply('Olá, me chamo Rob, e lhe darei informações sobre os estados do Brasil que voce pesquisar');
-    $bot->reply('Ah! e uma dica, voce pode digitar começando com "estado de/do/da" antes do nome do estado ou com o nome do estado e no final espaço e um ponto');
+    $bot->reply('Ah! e duas dicas, voce pode digitar começando com "estado de/do/da" antes do nome do estado ou somente o nome do estado e no final espaço e um ponto');
+    $bot->reply('e para uma melhor pesquisa digite os estados com acentuação correta'); 
     $bot->reply('qual estado é de seu interesse?');
 });
 
 $botman->hears('Olá', function ($bot) {
     $bot->reply('Olá, me chamo Rob, e lhe darei informações sobre os estados do Brasil que voce pesquisar');
-    $bot->reply('Ah! e uma dica, voce pode digitar começando com "estado de/do/da" antes do nome do estado ou com o nome do estado e no final espaço e um ponto');
+    $bot->reply('Ah! e duas dicas, voce pode digitar começando com "estado de/do/da" antes do nome do estado ou somente o nome do estado e no final espaço e um ponto');
+    $bot->reply('e para uma melhor pesquisa digite os estados com acentuação correta'); 
     $bot->reply('qual estado é de seu interesse?');
 });
 
 $botman->hears('Ola', function ($bot) {
     $bot->reply('Olá, me chamo Rob, e lhe darei informações sobre os estados do Brasil que voce pesquisar');
-    $bot->reply('Ah! e uma dica, voce pode digitar começando com "estado de/do/da" antes do nome do estado ou com o nome do estado e no final espaço e um ponto');
+    $bot->reply('Ah! e duas dicas, voce pode digitar começando com "estado de/do/da" antes do nome do estado ou somente o nome do estado e no final espaço e um ponto');
+    $bot->reply('e para uma melhor pesquisa digite os estados com acentuação correta'); 
     $bot->reply('qual estado é de seu interesse?');
 });
 
 $botman->hears('Fala', function ($bot) {
     $bot->reply('Olá, me chamo Rob, e lhe darei informações sobre os estados do Brasil que voce pesquisar');
-    $bot->reply('Ah! e uma dica, voce pode digitar começando com "estado de/do/da" antes do nome do estado ou com o nome do estado e no final espaço e um ponto');
+    $bot->reply('Ah! e duas dicas, voce pode digitar começando com "estado de/do/da" antes do nome do estado ou somente o nome do estado e no final espaço e um ponto');
+    $bot->reply('e para uma melhor pesquisa digite os estados com acentuação correta'); 
     $bot->reply('qual estado é de seu interesse?');
 });
 
 $botman->hears('Fale', function ($bot) {
     $bot->reply('Olá, me chamo Rob, e lhe darei informações sobre os estados do Brasil que voce pesquisar');
-    $bot->reply('Ah! e uma dica, voce pode digitar começando com "estado de/do/da" antes do nome do estado ou com o nome do estado e no final espaço e um ponto');
+    $bot->reply('Ah! e duas dicas, voce pode digitar começando com "estado de/do/da" antes do nome do estado ou somente o nome do estado e no final espaço e um ponto');
+    $bot->reply('e para uma melhor pesquisa digite os estados com acentuação correta'); 
     $bot->reply('qual estado é de seu interesse?');
 });
 
 $botman->hears('eai', function ($bot) {
     $bot->reply('Olá, me chamo Rob, e lhe darei informações sobre os estados do Brasil que voce pesquisar');
-    $bot->reply('Ah! e uma dica, voce pode digitar começando com "estado de/do/da" antes do nome do estado ou com o nome do estado e no final espaço e um ponto');
+    $bot->reply('Ah! e duas dicas, voce pode digitar começando com "estado de/do/da" antes do nome do estado ou somente o nome do estado e no final espaço e um ponto');
+    $bot->reply('e para uma melhor pesquisa digite os estados com acentuação correta'); 
     $bot->reply('qual estado é de seu interesse?');
 });
 
 
 $botman->hears('eae', function ($bot) {
     $bot->reply('Olá, me chamo Rob, e lhe darei informações sobre os estados do Brasil que voce pesquisar');
-    $bot->reply('Ah! e uma dica, voce pode digitar começando com "estado de/do/da" antes do nome do estado ou com o nome do estado e no final espaço e um ponto');
+    $bot->reply('Ah! e duas dicas, voce pode digitar começando com "estado de/do/da" antes do nome do estado ou somente o nome do estado e no final espaço e um ponto');
+    $bot->reply('e para uma melhor pesquisa digite os estados com acentuação correta'); 
     $bot->reply('qual estado é de seu interesse?');
 });
 
 $botman->hears('eaee', function ($bot) {
     $bot->reply('Olá, me chamo Rob, e lhe darei informações sobre os estados do Brasil que voce pesquisar');
-    $bot->reply('Ah! e uma dica, voce pode digitar começando com "estado de/do/da" antes do nome do estado ou com o nome do estado e no final espaço e um ponto');
+    $bot->reply('Ah! e duas dicas, voce pode digitar começando com "estado de/do/da" antes do nome do estado ou somente o nome do estado e no final espaço e um ponto');
+    $bot->reply('e para uma melhor pesquisa digite os estados com acentuação correta'); 
     $bot->reply('qual estado é de seu interesse?');
 });
 
 $botman->hears('falaa', function ($bot) {
     $bot->reply('Olá, me chamo Rob, e lhe darei informações sobre os estados do Brasil que voce pesquisar');
-    $bot->reply('Ah! e uma dica, voce pode digitar começando com "estado de/do/da" antes do nome do estado ou com o nome do estado e no final espaço e um ponto');
+    $bot->reply('Ah! e duas dicas, voce pode digitar começando com "estado de/do/da" antes do nome do estado ou somente o nome do estado e no final espaço e um ponto');
+    $bot->reply('e para uma melhor pesquisa digite os estados com acentuação correta'); 
     $bot->reply('qual estado é de seu interesse?');
 });
 
 $botman->hears('falee', function ($bot) {
     $bot->reply('Olá, me chamo Rob, e lhe darei informações sobre os estados do Brasil que voce pesquisar');
-    $bot->reply('Ah! e uma dica, voce pode digitar começando com "estado de/do/da" antes do nome do estado ou com o nome do estado e no final espaço e um ponto');
+    $bot->reply('Ah! e duas dicas, voce pode digitar começando com "estado de/do/da" antes do nome do estado ou somente o nome do estado e no final espaço e um ponto');
+    $bot->reply('e para uma melhor pesquisa digite os estados com acentuação correta'); 
     $bot->reply('qual estado é de seu interesse?');
 });
 
 $botman->hears('Oi,me chamo {nome}', function ($bot,$nome) {
     $bot->reply('Olá '.$nome.', me chamo Rob, e lhe darei informações sobre os estados do Brasil que voce pesquisar');
-    $bot->reply('Ah! e uma dica, voce pode digitar começando com "estado de/do/da" antes do nome do estado ou com o nome do estado e no final espaço e um ponto');
+    $bot->reply('Ah! e duas dicas, voce pode digitar começando com "estado de/do/da" antes do nome do estado ou somente o nome do estado e no final espaço e um ponto');
+    $bot->reply('e para uma melhor pesquisa digite os estados com acentuação correta'); 
     $bot->reply('qual estado é de seu interesse?');
 });
 
